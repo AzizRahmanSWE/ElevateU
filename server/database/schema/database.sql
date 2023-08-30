3df043259509855c64a092c84db771f74a9cdc0c
Friies <bahram.parsa2019@gmail.com>
2023-08-30 13:01:31 -0400

    create schema file

--- server/database/schema/database.sql
+++ server/database/schema/database.sql
@@ -12,6 +12,36 @@ CREATE TABLE users (
   profile_picture VARCHAR(225)
 );
 
+CREATE TABLE activities (
+  activity_id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  type VARCHAR(255),
+  targeted_muscle VARCHAR(50),
+  calories_burned_per_minute FLOAT
+);
+
+CREATE TABLE daily_logs ( 
+  log_id SERIAL PRIMARY KEY, 
+  user_id INTEGER REFERENCES users(user_id), 
+  activity_id INTEGER REFERENCES activities(activity_id), 
+  duration FLOAT, 
+  date DATE NOT NULL
+);
+
+CREATE TABLE notifications (
+  notification_id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(user_id),
+  message TEXT NOT NULL,
+  date DATE NOT NULL
+);
+
+CREATE TABLE exercise_demos (
+  demo_id SERIAL PRIMARY KEY,
+  activity_id INTEGER REFERENCES activities(activity_id),
+  video_link VARCHAR(255),
+  description TEXT
+);
+
 CREATE TABLE goals (
   goal_id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(user_id),
@@ -19,4 +49,22 @@ CREATE TABLE goals (
   start_date DATE,
   end_date DATE,
   status VARCHAR(50)
+);
+
+CREATE TABLE workout_presets (
+  workout_preset_id SERIAL PRIMARY KEY,
+  preset_name VARCHAR(255) NOT NULL,
+  duration INTEGER,
+  difficulty VARCHAR(50),
+  description TEXT
+);
+
+CREATE TABLE preset_activities (
+  preset_activity_id SERIAL PRIMARY KEY,
+  workout_preset_id INTEGER REFERENCES workout_presets(workout_preset_id),
+  activity_id INTEGER REFERENCES activities(activity_id),
+  day INTEGER,
+  sets INTEGER,
+  reps INTEGER,
+  duration FLOAT
 );
