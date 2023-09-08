1c85001c624f0830f2c7e11edbecf6bcfaa569ff
Jeremy Lee <jeremylee0615@gmail.com>
2023-09-08 02:09:58 -0700

    added more rendering & designs on Navbar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box } from "@mui/material";
 
 const Home = () => {
-  return <div>Home Hi Maiku</div>;
+  return <div>Home Hi Maiku PLEASE HEAL AND STOP BROILING</div>;
 };
 
 export default Home;
