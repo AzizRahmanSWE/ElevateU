f6bc535aca7c5161597bc6b81ea3a9fe2be942b4
Jeremy Lee <jeremylee0615@gmail.com>
2023-09-08 11:56:35 -0700

    add original Logo and reorganize folders

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,8 @@ import { Box } from "@mui/material";
 // components
 import Home from "./pages/Home";
 import ExerciseDetails from "./pages/ExerciseDetails";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./components/homePage/Navbar";
+import Footer from "./components/homePage/Footer";
 /*import About from './pages/About';
 import Goals from './pages/Goals';
 import SearchExercises from './pages/Exercises';*/
@@ -21,11 +21,13 @@ function App() {
       <Route path="/Goals" element={<Goals />} />
       <Route path="/Exercises" element={<SearchExercises />} />
     </Routes>*/
+
     <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetails />} />
+        {/* <Route path="/about" element={<About />} /> */}
       </Routes>
       <Footer />
     </Box>
