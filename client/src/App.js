56a129d4ee49c868a939298ab8f4526a32e849ea
Jeremy Lee <jeremylee0615@gmail.com>
2023-09-08 00:48:03 -0700

    set up for homepage design

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
 // components
-import Home from './pages/Home';
+/*import Home from './pages/Home';
 import About from './pages/About';
 import Goals from './pages/Goals';
-import SearchExercises from './pages/Exercises';
-
+import SearchExercises from './pages/Exercises';*/
 
 function App() {
   return (
-    <Routes>
+    /*<Routes>
       <Route path="/" element={<Home />} />
       <Route path="/About" element={<About />} />
       <Route path="/Home" element={<Home />} />
       <Route path="/Goals" element={<Goals />} />
       <Route path="/Exercises" element={<SearchExercises />} />
-    </Routes>
-  )
+    </Routes>*/
+    <Box width="400px">
+      Navbar
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExersiceDetail />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
