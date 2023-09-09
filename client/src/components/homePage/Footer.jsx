269552ebe5b74c5eba0c8d76997d79055c609f9b
Friies <bahram.parsa2019@gmail.com>
2023-09-09 13:11:23 -0400

    some css changes

--- client/src/components/homePage/Footer.jsx
+++ client/src/components/homePage/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Stack, Box, Typography } from "@mui/material";
+import Logo from "../../assets/images/Logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box mt="50px" bgcolor="lightpink">
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <img src={Logo} alt="logo" style={{ width: "110px", height: "100px", margin: "" }} />
+    </Stack>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="10px" textAlign="center" pb="40px">Contact Us</Typography>
+  </Box>
+  )
 };
 
 export default Footer;
