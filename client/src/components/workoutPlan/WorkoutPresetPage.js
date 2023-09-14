a426be605a19dd45166a0d18eded3b70e26b3028
Friies <bahram.parsa2019@gmail.com>
2023-09-14 11:55:55 -0400

    workout_presets page initial style and render.

---
+++ client/src/components/workoutPlan/WorkoutPresetPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ListSpecificWorkoutPresets from '../workoutPresets/ListSpecificWorkoutPresets';
+
+const WorkoutPresetPage = () => {
+  return (
+    <ListSpecificWorkoutPresets />
+  )
+}
+
+export default WorkoutPresetPage
